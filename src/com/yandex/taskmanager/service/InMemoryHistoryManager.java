72d0f357094d764ab86e37e78ae7716f0d9913ea
Sergeev-Alexander <8168619@yandex.ru>
2023-06-10 16:13:19 +0300

    fix: in method getHistory() made return a copy of historyList; feat: made c constant HISTORY_LIST_SIZE;

--- src/com/yandex/taskmanager/service/InMemoryHistoryManager.java
+++ src/com/yandex/taskmanager/service/InMemoryHistoryManager.java
@@ -3,20 +3,24 @@ package com.yandex.taskmanager.service;
 import com.yandex.taskmanager.model.Task;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
     private final LinkedList<Task> historyList = new LinkedList<>();
 
+    private final static int HISTORY_LIST_SIZE = 10;
+
     @Override
-    public List<Task> getHistory() {
-        return historyList;
+    public LinkedList<Task> getHistory() {
+        return new LinkedList<>(historyList);
     }
 
     @Override
     public void add(Task task) {
-        if (historyList.size() < 10) {
+        if (task == null) {
+            return;
+        }
+        if (historyList.size() < HISTORY_LIST_SIZE) {
             historyList.addFirst(task);
         } else {
             historyList.addFirst(task);
