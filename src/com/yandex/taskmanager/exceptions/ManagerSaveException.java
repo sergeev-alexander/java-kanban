c4c3515b25ed9fd5f932edca9b2dab0409814bd1
Sergeev-Alexander <8168619@yandex.ru>
2023-07-12 21:27:46 +0300

    final commit of sprint 6 homework

--- src/com/yandex/taskmanager/exceptions/ManagerSaveException.java
+++ src/com/yandex/taskmanager/exceptions/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package com.yandex.taskmanager.exceptions;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
 
     public ManagerSaveException(String message, Throwable cause) {
